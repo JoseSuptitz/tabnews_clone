82698efc96f3d04b3bfdfdb10d7e5212bc4b640f
José Carlos Suptitz <65187291+JoseSuptitz@users.noreply.github.com>
2023-12-21 15:05:37 +0000

    finaliza o endpoint `status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to api/v1/status/ should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const ResponseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(ResponseBody.updated_at).toISOString();
+  expect(ResponseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(ResponseBody.dependencies.database.version).toEqual("16.0");
+
+  expect(ResponseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(ResponseBody.dependencies.database.opened_connections).toEqual(1);
 });
